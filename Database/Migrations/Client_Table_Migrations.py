420c0419fdca2afdd017d9392e70b528ec315abe
MissNkabinde <135594937+MissNkabinde@users.noreply.github.com>
2023-08-15 12:31:43 +0200

    Revert "changes"
    This reverts commit 78350dd65a3ee31bfa8fc82ca7f00f463aa338bf.

--- Database/Migrations/Client_Table_Migrations.py
+++ Database/Migrations/Client_Table_Migrations.py
@@ -1,66 +1,37 @@
 from Database.Migrations.connection import create_connection
-import mysql.connector
-from mysql.connector import errorcode
-
-def table_exists(cursor, table_name):
-    cursor.execute("SHOW TABLES LIKE %s", (table_name,))
-    return cursor.fetchone() is not None
 
 def create_clients_table():
-    try:
-        conn = create_connection()
-        cursor = conn.cursor()
+    conn = create_connection()
+    cursor = conn.cursor()
 
-        table_name = 'clients'
+    query = """
+    CREATE TABLE clients (
+    id INTEGER PRIMARY KEY,
+    client_id INTEGER NOT NULL UNIQUE,
+    user_id INTEGER NOT NULL,
+    org_id INT NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    client_address VARCHAR(255) NOT NULL,
+    phone_no VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP NOT NULL
+);
 
-        if not table_exists(cursor, table_name):
-            query = """
-            CREATE TABLE clients (
-                id INTEGER PRIMARY KEY,
-                client_id INTEGER NOT NULL UNIQUE,
-                user_id INTEGER NOT NULL,
-                org_id INT NOT NULL,
-                first_name VARCHAR(255) NOT NULL,
-                last_name VARCHAR(255) NOT NULL,
-                email VARCHAR(255) NOT NULL,
-                client_address VARCHAR(255) NOT NULL,
-                phone_no VARCHAR(255) NOT NULL,
-                timestamp TIMESTAMP NOT NULL
-                FOREIGN KEY (org_id) REFERENCES organizations(id) ON DELETE CASCADE
+    """
+    cursor.execute(query)
+    conn.commit()
 
-            );
-            """
-            cursor.execute(query)
-            conn.commit()
-            print("Table 'clients' created successfully.")
-        else:
-            print("Table 'clients' already exists.")
-
-    except mysql.connector.Error as err:
-        print("Error creating table: {}".format(err))
-    finally:
-        if conn.is_connected():
-            cursor.close()
-            conn.close()
+    cursor.close()
+    conn.close()
 
 def drop_clients_table():
-    try:
-        conn = create_connection()
-        cursor = conn.cursor()
+    conn = create_connection()
+    cursor = conn.cursor()
 
-        query = "DROP TABLE IF EXISTS clients"
-        cursor.execute(query)
-        conn.commit()
+    query = "DROP TABLE IF EXISTS clients"
+    cursor.execute(query)
+    conn.commit()
 
-        print("Table 'clients' dropped successfully.")
-
-    except mysql.connector.Error as err:
-        print("Error dropping table: {}".format(err))
-    finally:
-        if conn.is_connected():
-            cursor.close()
-            conn.close()
-
-# Call the functions as needed
-create_clients_table()
-# drop_clients_table()
+    cursor.close()
+    conn.close()
--- Database/Migrations/Organization_Table_Migrations.py
+++ Database/Migrations/Organization_Table_Migrations.py
@@ -1,66 +1,39 @@
 from Database.Migrations.connection import create_connection
-import mysql.connector
-from mysql.connector import errorcode
-
-def table_exists(cursor, table_name):
-    cursor.execute("SHOW TABLES LIKE %s", (table_name,))
-    return cursor.fetchone() is not None
 
 def create_organizations_table():
-    try:
-        conn = create_connection()
-        cursor = conn.cursor()
+    conn = create_connection()
+    cursor = conn.cursor()
 
-        table_name = 'organizations'
+    query = """
+    CREATE TABLE organizations (
+    id INT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    org_id VARCHAR(255) NOT NULL,
+    user_id INT(20) UNSIGNED NOT NULL,
+    company_name VARCHAR(30) NOT NULL,
+    company_address VARCHAR(255) NOT NULL,
+    email_address VARCHAR(20) NOT NULL,
+    company_logo VARCHAR(20) NOT NULL,
+    phone_no TEXT NOT NULL,
+    website_link VARCHAR(255),
+    PRIMARY KEY (id),
+    UNIQUE (org_id)
+   
+)
 
-        if not table_exists(cursor, table_name):
-            query = """
-            CREATE TABLE organizations (
-                id INT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-                org_id VARCHAR(255) NOT NULL,
-                user_id INT(20) UNSIGNED NOT NULL,
-                company_name VARCHAR(30) NOT NULL,
-                company_address VARCHAR(255) NOT NULL,
-                email_address VARCHAR(20) NOT NULL,
-                company_logo VARCHAR(20) NOT NULL,
-                phone_no TEXT NOT NULL,
-                website_link VARCHAR(255),
-                PRIMARY KEY (id),
-                UNIQUE (org_id)
-                FOREIGN KEY (user_id) REFERENCES users(id)
-            )
-            """
-            cursor.execute(query)
-            conn.commit()
-            print("Table 'organizations' created successfully.")
-        else:
-            print("Table 'organizations' already exists.")
+    """
+    cursor.execute(query)
+    conn.commit()
 
-    except mysql.connector.Error as err:
-        print("Error creating table: {}".format(err))
-    finally:
-        if conn.is_connected():
-            cursor.close()
-            conn.close()
+    cursor.close()
+    conn.close()
 
 def drop_organizations_table():
-    try:
-        conn = create_connection()
-        cursor = conn.cursor()
+    conn = create_connection()
+    cursor = conn.cursor()
 
-        query = "DROP TABLE IF EXISTS organizations"
-        cursor.execute(query)
-        conn.commit()
+    query = "DROP TABLE IF EXISTS organizations"
+    cursor.execute(query)
+    conn.commit()
 
-        print("Table 'organizations' dropped successfully.")
-
-    except mysql.connector.Error as err:
-        print("Error dropping table: {}".format(err))
-    finally:
-        if conn.is_connected():
-            cursor.close()
-            conn.close()
-
-# Call the functions as needed
-create_organizations_table()
-# drop_organizations_table()
+    cursor.close()
+    conn.close()
