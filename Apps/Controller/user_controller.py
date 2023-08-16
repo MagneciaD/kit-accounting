40a3a5f7910bbc733db54c670ffc074547f86bfc
KrayATM <129862197+KrayATM@users.noreply.github.com>
2023-08-16 14:42:36 +0200

    Users Model & Controller
    Created CRUD for Users Model
    Added inputs for save function

--- Apps/Controller/user_controller.py
+++ Apps/Controller/user_controller.py
@@ -1,116 +1,13 @@
-from Database.Migrations.connection import create_connection
-import mysql.connector
-from mysql.connector import errorcode
+from Apps.Model.User import User
 
-def table_exists(cursor, table_name):
-    cursor.execute("SHOW TABLES LIKE %s", (table_name,))
-    return cursor.fetchone() is not None
+def create_user_from_input():
+    name = input("Enter your name: ")
+    email = input("Enter your email: ")
+    password = input("Enter your password: ")
 
-def create_user(user_data):
-    try:
-        conn = create_connection()
-        cursor = conn.cursor()
+    user = User(name, email, password)
+    user.save()
 
-        query = """
-        INSERT INTO users (user_id, name, email, password)
-        VALUES (%s, %s, %s, %s)
-        """
-        data = (user_data['user_id'], user_data['name'], user_data['email'], user_data['password'])
-        cursor.execute(query, data)
-        conn.commit()
+create_user_from_input()
 
-        print("User created successfully.")
 
-    except mysql.connector.Error as err:
-        print("Error creating user:", err)
-    finally:
-        if conn.is_connected():
-            cursor.close()
-            conn.close()
-
-def read_user(user_id):
-    try:
-        conn = create_connection()
-        cursor = conn.cursor(dictionary=True)
-
-        query = "SELECT * FROM users WHERE user_id = %s"
-        cursor.execute(query, (user_id,))
-        user = cursor.fetchone()
-
-        if user:
-            return user
-        else:
-            print("User not found.")
-            return None
-
-    except mysql.connector.Error as err:
-        print("Error reading user:", err)
-    finally:
-        if conn.is_connected():
-            cursor.close()
-            conn.close()
-
-def update_user(user_id, new_data):
-    try:
-        conn = create_connection()
-        cursor = conn.cursor()
-
-        query = """
-        UPDATE users
-        SET name = %s, email = %s, password = %s
-        WHERE user_id = %s
-        """
-        data = (new_data['name'], new_data['email'], new_data['password'], user_id)
-        cursor.execute(query, data)
-        conn.commit()
-
-        print("User updated successfully.")
-
-    except mysql.connector.Error as err:
-        print("Error updating user:", err)
-    finally:
-        if conn.is_connected():
-            cursor.close()
-            conn.close()
-
-def delete_user(user_id):
-    try:
-        conn = create_connection()
-        cursor = conn.cursor()
-
-        query = "DELETE FROM users WHERE user_id = %s"
-        cursor.execute(query, (user_id,))
-        conn.commit()
-
-        print("User deleted successfully.")
-
-    except mysql.connector.Error as err:
-        print("Error deleting user:", err)
-    finally:
-        if conn.is_connected():
-            cursor.close()
-            conn.close()
-
-# Call the functions as needed
-# create_users_table()
-# drop_users_table()
-
-# Example usage:
-# create_user({
-#     'user_id': 'user123',
-#     'name': 'John Doe',
-#     'email': 'john@example.com',
-#     'password': 'securepass'
-# })
-
-# user = read_user('user123')
-# if user:
-#     print("User:", user)
-
-# update_user('user123', {
-#     'name': 'Jane Smith',
-#     'email': 'jane@example.com',
-#     'password': 'newpass'
-# })
-
-# delete_user('user123')
--- Apps/Model/User.py
+++ Apps/Model/User.py
@@ -22,3 +22,62 @@ class User:
 
         cursor.close()
         conn.close()
+
+    def update(self):
+        conn = create_connection()
+        cursor = conn.cursor()
+
+        query = "UPDATE users SET name = %s, email = %s, password = %s WHERE id = %s"
+        values = (self.name, self.email, self.password, self.id)  # Assuming you have an 'id' attribute for each user
+
+        try:
+            cursor.execute(query, values)
+            conn.commit()
+            print("User updated successfully!")
+        except Exception as e:
+            print(f"Error updating user: {e}")
+
+        cursor.close()
+        conn.close()
+
+    @staticmethod
+    def read(user_id):
+        conn = create_connection()
+        cursor = conn.cursor()
+
+        query = "SELECT id, name, email FROM users WHERE id = %s"
+        values = (user_id,)
+
+        try:
+            cursor.execute(query, values)
+            user_data = cursor.fetchone()
+            if user_data:
+                user = User(user_data[1], user_data[2], '')  # Pass an empty string for password for security reasons
+                user.id = user_data[0]
+                return user
+            else:
+                print("User not found.")
+                return None
+        except Exception as e:
+            print(f"Error reading user: {e}")
+            return None
+        finally:
+            cursor.close()
+            conn.close()
+
+    def delete(self):
+        conn = create_connection()
+        cursor = conn.cursor()
+
+        query = "DELETE FROM users WHERE id = %s"
+        values = (self.id,)  # Assuming you have an 'id' attribute for each user
+
+        try:
+            cursor.execute(query, values)
+            conn.commit()
+            print("User deleted successfully!")
+        except Exception as e:
+            print(f"Error deleting user: {e}")
+
+        cursor.close()
+        conn.close()
