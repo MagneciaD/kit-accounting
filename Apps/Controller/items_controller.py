3a26095825e6703c6ab8458ef5bfa40621106ccd
MissNkabinde <135594937+MissNkabinde@users.noreply.github.com>
2023-08-18 14:31:36 +0200

    items models and item_controller

---
+++ Apps/Controller/items_controller.py
@@ -0,0 +1,90 @@
+# Import the User class from the correct module
+import random
+import string
+from Apps.Model.items import Item
+
+def generate_qi_id():
+    numbers = ''.join(random.choices(string.digits, k=5))
+    letters = ''.join(random.choices(string.ascii_letters, k=2))
+    qi_id = 'QI' + numbers
+    return qi_id
+
+def create_item_from_input():
+    try:
+        user_id = input("Enter user ID: ")
+        name = input("Enter item name: ")
+        item_quality = input("Enter item quality: ")
+        price = float(input("Enter item price: "))
+        sub_total = float(input("Enter sub total: "))
+        qi_id = generate_qi_id()
+
+        # Create an Item instance
+        item = Item(qi_id, user_id, name, item_quality, price, sub_total)
+
+        # Assuming the Item class has a save() method
+        item.save()
+
+        print("Item created and saved successfully!")
+
+    except Exception as e:
+        print("An error occurred:", e)
+
+def read_all_items():
+    items = Item.read_all()
+
+    if items:
+        for item in items:
+            print(f"Item ID: {item.qi_id}")
+            print(f"User ID: {item.user_id}")
+            print(f"Name: {item.name}")
+            print(f"Item Quality: {item.item_quality}")
+            print(f"Price: {item.price}")
+            print(f"Sub Total: {item.sub_total}")
+            print()  # Add a blank line between items
+    else:
+        print("No items found.")
+
+def read_item_by_qi_id():
+    qi_id = input("Enter the item ID you want to read: ")
+
+    item = Item.read_by_qi_id(qi_id)
+
+    if item:
+        print(f"Item ID: {item.qi_id}")
+        print(f"User ID: {item.user_id}")
+        print(f"Name: {item.name}")
+        print(f"Item Quality: {item.item_quality}")
+        print(f"Price: {item.price}")
+        print(f"Sub Total: {item.sub_total}")
+    else:
+        print("Item not found.")
+
+def update_item_from_input(qi_id, user_id):
+    name = input("Enter new item name: ")
+    item_quality = input("Enter new item quality: ")
+    price = float(input("Enter new item price: "))
+    sub_total = float(input("Enter new sub total: "))
+
+    item = Item(qi_id, user_id, name, item_quality, price, sub_total)  # Pass all required arguments
+    item.update()
+
+def delete_item_by_qi_id(qi_id):
+    Item.delete_by_qi_id(qi_id)
+    print(f"Item with ID {qi_id} has been deleted.")
+
+# Call the create_item_from_input() function to create a new item
+create_item_from_input()
+
+# Call the function to read and display all items
+# read_all_items()
+
+# Call the read_item_by_qi_id() function to read an item by its ID
+read_item_by_qi_id()
+
+# Call the update_item_from_input() function to update an item
+item_qi_id_to_update = input("Enter the item ID you want to update: ")
+update_item_from_input(item_qi_id_to_update)
+
+# Prompt the user to enter the ID of the item they want to delete
+item_qi_id_to_delete = input("Enter the ID of the item you want to delete: ")
+delete_item_by_qi_id(item_qi_id_to_delete)
--- Apps/Model/items.py
+++ Apps/Model/items.py
@@ -1,7 +1,7 @@
 from Database.Migrations.connection import create_connection
 import mysql.connector
 
-class Items:
+class Item:
     def __init__(self, qi_id, user_id, name, item_quality, price, sub_total):
         self.qi_id = qi_id
         self.user_id = user_id
@@ -11,29 +11,106 @@ class Items:
         self.sub_total = sub_total
 
     def save(self):
+        conn = create_connection()
+        cursor = conn.cursor()
+
+        query = """
+        INSERT INTO items (qi_id, user_id, name, item_quality, price, sub_total)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        values = (self.qi_id, self.user_id, self.name, self.item_quality, self.price, self.sub_total)
+
         try:
-            conn = create_connection()
-            cursor = conn.cursor()
-
-            query = """
-            INSERT INTO items (qi_id, user_id, name, item_quality, price, sub_total)
-            VALUES (%s, %s, %s, %s, %s, %s)
-            """
-            values = (self.qi_id, self.user_id, self.name, self.item_quality, self.price, self.sub_total)
-
             cursor.execute(query, values)
             conn.commit()
-            print("Item saved successfully.")
+            print("Item saved successfully!")
+        except Exception as e:
+            print(f"Error saving item: {e}")
 
-        except mysql.connector.Error as err:
-            print("Error saving item: {}".format(err))
+        cursor.close()
+        conn.close()
+
+    def update(self):
+        conn = create_connection()
+        cursor = conn.cursor()
+
+        query = """
+        UPDATE items
+        SET user_id = %s, name = %s, item_quality = %s, price = %s, sub_total = %s
+        WHERE qi_id = %s
+        """
+        values = (self.user_id, self.name, self.item_quality, self.price, self.sub_total, self.qi_id)
+
+        try:
+            cursor.execute(query, values)
+            conn.commit()
+            print("Item updated successfully!")
+        except Exception as e:
+            print(f"Error updating item: {e}")
         finally:
-            if conn.is_connected():
-                cursor.close()
-                conn.close()
+            cursor.close()
+            conn.close()
 
-    # Implement other CRUD methods like update, read, and delete here
+    @staticmethod
+    def read_all():
+        conn = create_connection()
+        cursor = conn.cursor()
 
-# Example usage:
-item = Items("qi123", "user123", "Item Name", 3, "10.99", "32.97")
-item.save()
+        query = "SELECT qi_id, user_id, name, item_quality, price, sub_total FROM items"
+
+        try:
+            cursor.execute(query)
+            item_data_list = cursor.fetchall()
+            items = []
+            for item_data in item_data_list:
+                item = Item(item_data[0], item_data[1], item_data[2], item_data[3], item_data[4], item_data[5])
+                items.append(item)
+            return items
+        except Exception as e:
+            print(f"Error reading items: {e}")
+            return []
+        finally:
+            cursor.close()
+            conn.close()
+
+    @staticmethod
+    def read_by_qi_id(qi_id):
+        conn = create_connection()
+        cursor = conn.cursor()
+
+        query = "SELECT qi_id, user_id, name, item_quality, price, sub_total FROM items WHERE qi_id = %s"
+        values = (qi_id,)
+
+        try:
+            cursor.execute(query, values)
+            item_data = cursor.fetchone()
+            if item_data:
+                item = Item(item_data[0], item_data[1], item_data[2], item_data[3], item_data[4], item_data[5])
+                return item
+            else:
+                print("Item not found.")
+                return None
+        except Exception as e:
+            print(f"Error reading item: {e}")
+            return None
+        finally:
+            cursor.close()
+            conn.close()
+
+    @staticmethod
+    def delete_by_qi_id(qi_id):
+        conn = create_connection()
+        cursor = conn.cursor()
+
+        query = "DELETE FROM items WHERE qi_id = %s"
+        values = (qi_id,)
+
+        try:
+            cursor.execute(query, values)
+            conn.commit()
+            print("Item deleted successfully!")
+        except Exception as e:
+            print(f"Error deleting item: {e}")
+        finally:
+            cursor.close()
+            conn.close()
