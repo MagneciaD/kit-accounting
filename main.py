6784a4c5a27a56667fb2f2d1e4ad5505ca9de784
KrayATM <129862197+KrayATM@users.noreply.github.com>
2023-08-24 09:56:13 +0200

    Switch Statement Structure to main
    changed else if to switch case and re ordered the code

--- main.py
+++ main.py
@@ -3,31 +3,39 @@ from Resources.View.login import *
 from Resources.View.dashboard_view import *
 
 
-
 def display_menu():
     print("Menu:")
-    print("1. Login")
-    print("2. Register")
-    print("3. Log out")
+    print("1. Open Login")
+    print("2. Option 2")
+    print("3. Exit")
+
+def open_login():
+    print("Opening Login...")
+    fill_in()
+
+def option_2():
+    print("You selected Option 2.")
+    # Perform actions for Option 2
+    create_user_from_input()
+
+def exit_program():
+    print("Exiting the program.")
+    return True
 
 def main():
+    options = {
+        "1": open_login,
+        "2": option_2,
+        "3": exit_program
+    }
+
     while True:
         display_menu()
         choice = input("Enter your choice: ")
 
-        if choice == "1":
-            print("Opening Login...")
-            fill_in()
-
-        elif choice == "2":
-            print("You selected Option 2.")
-            # Perform actions for Option 2
-            create_user_from_input()
-
-        elif choice == "3":
-            print("Exiting the program.")
-            break
-
+        if choice in options:
+            if options[choice]():
+                break
         else:
             print("Invalid choice. Please select a valid option.")
 
